4d6a8840b7c35df459ea9eb37c03ad905fea88a3
Rupam Barui <rupambarui.17@gmail.com>
2024-06-06 18:29:59 +0530

    Initial Project setup with react, material ui, tailwindcss

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+
+function App() {
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={<Layout />}
+				>
+				<Route
+					index
+					element={<Home />}
+				/>
+				{/* <Route
+					path="/contact-us"
+					element={<Contact />}
+				/> */}
+			</Route>
+		</Routes>
+	);
+}
+
+export default App;
---
+++ src/Layout.js
@@ -0,0 +1,33 @@
+import { Container } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import NavBar from "./components/NavBar/NavBar";
+import PreLoader from "./components/PreLoader/PreLoader";
+
+export default function Layout() {
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const delay = () => {
+			setTimeout(() => {
+				setIsLoading(false);
+			}, 1650);
+		};
+		delay();
+	}, []);
+
+	return isLoading ? (
+		<PreLoader />
+	) : (
+		<div className="body">
+			<NavBar />
+			<main className="pt-24 min-h-[95vh]">
+				<Container>
+					<Outlet />
+				</Container>
+			</main>
+			<Footer />
+		</div>
+	);
+}
