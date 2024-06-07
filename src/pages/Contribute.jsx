d16813f2bad96cb43d4b75ecedff6112f3d98204
Rupam Barui <rupambarui.17@gmail.com>
2024-06-07 10:30:46 +0530

    styles typo fix

--- src/pages/Contribute.jsx
+++ src/pages/Contribute.jsx
@@ -41,8 +41,8 @@ const Contribute = () => {
 
 	return (
 		<div className="min-h-screen flex flex-col">
-			<header className="bg-gray-900 text-white py-8">
-				<div className="container mx-auto px=4">
+            <header className="bg-gray-900 text-white py-8">
+				<div className="container mx-auto px-4">
 					<h1 className="text-3xl font-bold">Contribute</h1>
 				</div>
 			</header>
